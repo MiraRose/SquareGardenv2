a1b854c4083b978d786dc8e24d9088da9e476fd5
MiraRose <miraastar@gmail.com>
2023-12-04 20:16:23 -0500

    Make a square of squares

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,39 @@
 
-  import {useState} from 'react';
+import { useState } from 'react';
 
-      function Header({title}) {
-        return(<h1>{title}</h1>)
-      }
+function Header({ title }) {
+  return (<h1>{title}</h1>)
+}
 
-      function SideMenu() {
-        return(
-          <div class="side-menu">
-             <Header title="Square Gardening v2" />
-          </div>
-        )
-      }
+function SideMenu() {
+  return (
+    <div className="side-menu">
+      <Header title="Square Gardening v2" />
+    </div>
+  )
+}
 
-      export default function HomePage() {
-        return(
-          <div>
-           
-            <SideMenu/>
-            
-          </div>
-        );
-      }
+function Square({ id }) {
+  return (
+    <div className="square" key={id} id={id}>
+    </div>
+  )
+}
+
+export default function HomePage() {
+  let squares = []
+  for (let i = 0; i <= 15; i++) {
+    squares.push(<Square id={i} />)
+  }
+  return (
+    <div>
+     
+      <SideMenu />
+      <div className="garden-bed">
+        {squares}
+      </div>
+
+    </div>
+  );
+}
 
