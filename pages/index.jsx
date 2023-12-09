14692bc30044bae317155dcc17402d515ad1f80a
MiraRose <miraastar@gmail.com>
2023-12-08 21:43:21 -0500

    Now you can drag a veg to a sq, woo!

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,51 +6,87 @@ function Header({ title }) {
   return (<h1>{title}</h1>)
 }
 
-function SideMenu() {
+function SideMenu({onDragStart}) {
   return (
     <div className="side-menu">
       <Header title="Square Gardening v2" />
-      <Vegetables/>
+      <Vegetables onDragStart={onDragStart}/>
     </div>
   )
 }
-function Vegetables() {
-  const vegetables = ["basil", "bell-pepper", "broccoli", "cabbage", "carrot", "celery", "chamomile", "corn", "cucumber", "dill", "eggplant", "fennel", "garlic", "green-onion", "beans", "lemongrass", "lettuce", "marigold", "melon", "mint", "onion", "oregano", "parsley", "peas", "potato", "pumpkin", "radish", "rosemary", "spinach", "strawberry"]
+
+function Vegetables({onDragStart}) {
+  const vegetables = ["basil", "bell-pepper", "broccoli", "cabbage", "carrot", "celery", "chamomile", "corn", "cucumber", "dill", "eggplant", "fennel", "garlic", "green-onion", "beans", "lemongrass", "lettuce", "marigold", "melon", "mint", "onion", "oregano", "parsley", "peas", "potato", "pumpkin", "radish", "rosemary", "tomato", "spinach", "strawberry"]
   return (
     <div className="vegetables">
       {vegetables.map((vegetable) => (
-      <Vegetable name={vegetable}/>
+      <Vegetable name={vegetable} onDragStart={onDragStart}/>
     ))
     }
     </div>
   )
 }
 
-function Vegetable({name}) {
+function Vegetable({name, onDragStart}) {
   const filepath = "veggies/" + name + ".svg"
+
   return (
     <div key={name} id={name}>
-      <Image src={filepath}  width="50" height="50" />
+      <Image src={filepath} id={name} width="50" height="50" onDragStart={onDragStart} />
     </div>
   )
 }
 
-function Square({ id }) {
+function Square({ id, chosenVeg }) {
+  function handleDragLeave(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    console.log("Drag leave")
+  };
+
+  function handleDragOver(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    console.log("Drag over")
+  };
+  function handleDragEnter(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    console.log("Drag enter")
+  };
+
+  function handleDrop(event) {
+    event.stopPropagation()
+    event.preventDefault()
+    let square = document.getElementById("square" + id)
+    let img = document.createElement("img")
+    img.src = "veggies/" + chosenVeg + ".svg"
+    img.className = "plantedVeg"
+    square.appendChild(img)
+    
+  };
+  const sqId = "square" + id
   return (
-    <div className="square" key={id} id={id}>
+    <div className="square" onDragOver={handleDragOver} onDragEnter={handleDragEnter} onDragLeave={handleDragLeave} onDrop={handleDrop} key={id} id={sqId}>
     </div>
   )
 }
 
 export default function HomePage() {
+  const [chosenVeg, setChosenVeg] = useState()
+
+  function handleDragStart(e) {
+    setChosenVeg(e.target.id)
+  }
+
   let squares = []
   for (let i = 0; i <= 15; i++) {
-    squares.push(<Square id={i} />)
+    squares.push(<Square id={i} chosenVeg={chosenVeg} />)
   }
   return (
     <div>
      
-      <SideMenu />
+      <SideMenu onDragStart={(e) => handleDragStart(e)} />
       <div className="garden-bed">
         {squares}
       </div>
